9af54be128d20bda2774e4caa2c6b5aefb13d05e
o-j2315 <J0sue.Hernande2@hotmail.com>
2025-07-11 22:49:10 -0600

    fix: update host configuration in Swagger documentation

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "https://cse341-backend-jvsu.onrender.com",
+  host: "cse341-backend-jvsu.onrender.com",
   schemes: ["https"]
 };
 
